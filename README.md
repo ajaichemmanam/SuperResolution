f01446be692b47555c6b78377094b22364d7c3d1
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2020-08-23 19:05:28 +0530

    code cleanup and added output

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SuperResolution
+
 A Super Resolution model built with pytorch lightning
+
+## Output
+
+![Input Image](https://raw.githubusercontent.com/ajaichemmanam/SuperResolution/master/input.jpg)
+
+![Output Image](https://raw.githubusercontent.com/ajaichemmanam/SuperResolution/master/output.jpg)
--- demo.py
+++ demo.py
@@ -4,29 +4,17 @@ import argparse
 import torch
 from PIL import Image
 from torchvision.transforms import ToTensor
-# from main import SRModel
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+original_image = "./dataset/BSDS300/images/test/3096.jpg"
+input_image = "./input.jpg"
+output_image = "./output.jpg"
 
-# Training settings
-# parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
-# parser.add_argument('--input_image', type=str,
-#                     required=True, help='input image to use')
-# parser.add_argument('--model', type=str, required=True,
-#                     help='model file to use')
-# parser.add_argument('--output_filename', type=str,
-#                     help='where to save the output image')
-# parser.add_argument('--cuda', action='store_true', help='use cuda')
-# opt = parser.parse_args()
-
-# print(opt)
-input_image = "/Users/ajaichemmanam/Documents/Projects/PytorchLightning/SR/dataset/BSDS300/images/test/3096.jpg"
-output_file = "/Users/ajaichemmanam/Documents/Projects/PytorchLightning/SR/output.jpg"
-# modelPath = "/Users/ajaichemmanam/Documents/Projects/PytorchLightning/SR/lightning_logs/version_0/checkpoints/epoch=1.ckpt"
-
-img = Image.open(input_image)
+img = Image.open(original_image)
 img = img.resize((224, 224))
+img.save(input_image)
 img_ycbcr = img.convert('YCbCr')
 y, cb, cr = img_ycbcr.split()
 img_ndarray = np.asarray(y)
@@ -34,7 +22,7 @@ img_4 = np.expand_dims(np.expand_dims(img_ndarray, axis=0), axis=0)
 imginput = img_4.astype(np.float32) / 255.0
 print(imginput.shape)
 
-ort_session = onnxruntime.InferenceSession('model.onnx')
+ort_session = onnxruntime.InferenceSession('./model.onnx')
 input_name = ort_session.get_inputs()[0].name
 ort_inputs = {input_name: imginput}
 ort_outs = ort_session.run(None, ort_inputs)
@@ -53,5 +41,5 @@ out_img = Image.merge(
         cr.resize(ort_outs.size, Image.BICUBIC),
     ]).convert("RGB")
 plt.imshow(out_img)
-out_img.save(output_file)
-print('output image saved to ', output_file)
+out_img.save(output_image)
+print('output image saved to ', output_image)
